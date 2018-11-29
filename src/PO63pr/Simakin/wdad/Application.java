a917c5109ec983ccfa0fa351859dd3da8f1be586
PSYSHO <giks1998@mail.ru>
2018-11-29 14:23:22 +0400

    Added entry point

---
+++ src/PO63pr/Simakin/wdad/Application.java
@@ -0,0 +1,7 @@
+package PO63pr.Simakin.wdad;
+public class Application {
+    public static void  main(String[] Args){
+        String first = "I'm Konstantin Simakin,and I'm not monkey -_-";
+        System.out.print(first);
+    }
+}
